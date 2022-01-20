1e08b4c44f89d127887c8b766f2601e7a8d9d101
mmuellersoppart <mmuellersoppart@gmail.com>
2022-01-20 08:34:47 -0600

    re indent code, and add comments to authentication controller

--- Sources/App/Controllers/AuthenticationWebsiteController.swift
+++ Sources/App/Controllers/AuthenticationWebsiteController.swift
@@ -12,7 +12,7 @@ struct AuthenticationWebsiteController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         routes.get("register", use: registerHandler)
         routes.post("register", use: registerPostHandler)
-
+        
         routes.get("login", use: loginHandler)
         
         let basicAuthRoutes = routes.grouped(
@@ -23,33 +23,45 @@ struct AuthenticationWebsiteController: RouteCollection {
         basicAuthRoutes.post("logout", use: logoutHandler)
     }
     
+    /// Renders registration page
     func registerHandler(_ req: Request) async throws -> View {
-    
-      let baseContext = BaseContext(title: "Register", isLoggedIn: false)
         
-      let context = RegisterContext(baseContext: baseContext)
-      return try await req.view.render("register", context)
+        let baseContext = BaseContext(title: "Register", isLoggedIn: false)
+        let context = RegisterContext(baseContext: baseContext)
+        
+        return try await req.view.render("register", context)
     }
     
+    /// Creates user from registration form data
     func registerPostHandler(_ req: Request) async throws -> Response {
-      let data = try req.content.decode(RegisterData.self)
-      let password = try Bcrypt.hash(data.password)
-      let user = User(
-        id: UUID(),
-        username: data.username,
-        email: data.email,
-        password: password
-      )
-      try await user.save(on: req.db)
- 
-      req.auth.login(user)
-      return req.redirect(to: "/")
+        
+        // convert data from form to RegisterDTO object
+        let data = try req.content.decode(RegisterDTO.self)
+        
+        // convert plaintext password to an encrypted one and save that!
+        let password = try Bcrypt.hash(data.password)
+        
+        let user = User(
+            id: UUID(),
+            username: data.username,
+            email: data.email,
+            password: password
+        )
+        try await user.save(on: req.db)
+        
+        // login user, they'll recieve a session in a cookie now
+        req.auth.login(user)
+        
+        return req.redirect(to: "/")
     }
     
+    /// renders login page
     func loginHandler(_ req: Request) async throws -> View {
         let baseContext = BaseContext(title: "Login", isLoggedIn: false)
         let context: LoginContext
         
+        // if error exist, a notification will appear on screen telling
+        // the user.
         if let error = req.query[Bool.self, at: "error"], error {
             context = LoginContext(baseContext: baseContext, loginError: true)
         } else {
@@ -59,23 +71,27 @@ struct AuthenticationWebsiteController: RouteCollection {
         return try await req.view.render("login", context)
     }
     
+    /// takes information from login form and logs in users who are in auth cache (currently only in memory)
     func loginPostHandler(_ req: Request) async throws -> Response {
         do {
+            // case where user is found in the system
             let user = try req.auth.require(User.self)
             req.auth.login(user)
             return req.redirect(to: "/")
         } catch {
+            // user is not in the system, so we send an error in the context
             let baseContext = BaseContext(title: "Login", isLoggedIn: false)
             let context = LoginContext(baseContext: baseContext, loginError: true)
             return try await req.view.render("login", context).encodeResponse(for: req)
         }
     }
     
+    /// Logs out any user who is currently logged in. 
     func logoutHandler(_ req: Request) -> Response {
-      req.auth.logout(User.self)
-      return req.redirect(to: "/")
+        req.auth.logout(User.self)
+        return req.redirect(to: "/")
     }
-
+    
 }
 
 // Log in
@@ -99,9 +115,9 @@ struct RegisterContext: Encodable {
     let baseContext: BaseContext
 }
 
-struct RegisterData: Content {
-  let username: String
-  let email: String
-  let password: String
-  let confirmPassword: String
+struct RegisterDTO: Content {
+    let username: String
+    let email: String
+    let password: String
+    let confirmPassword: String
 }
